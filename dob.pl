89f3989959acf10c2b9831e09e3759634801da58
Balu1815 <garlapatibalaji2005@gmail.com>
2026-06-18 13:28:30 +0530

    Create dob.pl

---
+++ dob.pl
@@ -0,0 +1,25 @@
+Q27 — Database with NAME and DOB
+Code
+
+person(john,'10-01-2004').
+person(ravi,'22-06-2003').
+person(partha,'15-09-2004').
+
+
+
+Input 1
+person(john,D).
+Output
+D = '10-01-2004'
+
+Input 2
+
+person(X,'22-06-2003').
+
+Output
+
+X = ravi
+
+Rule:
+
+Unknown value = variable (capital)
