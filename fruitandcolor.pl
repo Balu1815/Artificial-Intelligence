fe4beca6e7c3a86b660841ae583de0ebab1fafc5
Balu1815 <garlapatibalaji2005@gmail.com>
2026-06-18 13:33:50 +0530

    Create fruitandcolor.pl with fruit-color facts
    Add Prolog code for fruit and color relationships.

---
+++ fruitandcolor.pl
@@ -0,0 +1,17 @@
+Q35 — Fruit and Color (Backtracking)
+Code
+fruit(apple,red).
+fruit(banana,yellow).
+fruit(grapes,green).
+Input
+fruit(X,red).
+
+Output
+
+X = apple
+
+Press:
+
+;
+
+(check more solutions)
