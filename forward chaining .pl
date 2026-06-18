392d929e9bfdfbe650eec97836f7e3b1f293f885
Balu1815 <garlapatibalaji2005@gmail.com>
2026-06-18 13:30:02 +0530

    Create forward chaining .pl

---
+++ forward chaining .pl
@@ -0,0 +1,35 @@
+
+
+Code:
+
+rain.
+
+wet_ground :-
+    rain.
+
+drive_slow :-
+    wet_ground.
+
+Meaning:
+
+If rain → ground wet
+
+If ground wet → drive slow
+
+Query/Input
+
+Ask:
+
+wet_ground.
+
+Output:
+
+true.
+
+Ask:
+
+drive_slow.
+
+Output:
+
+true.
